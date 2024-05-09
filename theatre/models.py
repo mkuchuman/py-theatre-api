616671cf8c730026c0f02a9e464e3ec9e33df27d
mkuchuman <mkuchuman1@gmail.com>
2024-05-09 23:40:17 +0200

    created full name property

--- theatre/models.py
+++ theatre/models.py
@@ -7,6 +7,10 @@ class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
 
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
 
 class Genre(models.Model):
     name = models.CharField(max_length=255)
