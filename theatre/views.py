d3dc1b96734b78cf901b6ce5c227f639188d2ffc
mkuchuman <mkuchuman1@gmail.com>
2024-05-09 23:41:23 +0200

    created views for actors and genres

--- theatre/views.py
+++ theatre/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
 
-# Create your views here.
+from theatre.models import Genre, Actor
+from theatre.serializers import GenreSerializer, ActorSerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    # permission_classes = ()
+
+
+class ActorViewSet(viewsets.ModelViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+    # permission_classes = ()
