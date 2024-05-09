4e0bd6fad2d26c6fdfb4125eb04a98750fa2a9ff
mkuchuman <mkuchuman1@gmail.com>
2024-05-09 23:40:45 +0200

    created serizlizers for actors and genres

---
+++ theatre/serializers.py
@@ -0,0 +1,14 @@
+from theatre.models import Genre, Actor
+from rest_framework import serializers
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ("id", "name")
+
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = ("id", "first_name", "last_name", "full_name",)
