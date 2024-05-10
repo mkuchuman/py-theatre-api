03120f9dfeae32fd04c6933cb15bb91faae139c1
mkuchuman <mkuchuman1@gmail.com>
2024-05-10 15:58:26 +0200

    TheatreHallViewSet registred in router

--- theatre/urls.py
+++ theatre/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from theatre.views import GenreViewSet, ActorViewSet
+from theatre.views import GenreViewSet, ActorViewSet, TheatreHallViewSet
 
 app_name = "theatre"
 
 router = DefaultRouter()
 router.register("genres", GenreViewSet)
 router.register("actors", ActorViewSet)
+router.register("theatre_halls", TheatreHallViewSet)
 
 urlpatterns = [
     path("", include(router.urls))
