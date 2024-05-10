571f4469bcc0b2166b7beb61026ccbbd575c3453
mkuchuman <mkuchuman1@gmail.com>
2024-05-10 18:37:13 +0200

    registred performance view in router

--- theatre/urls.py
+++ theatre/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from theatre.views import GenreViewSet, ActorViewSet, TheatreHallViewSet
+from theatre.views import GenreViewSet, ActorViewSet, TheatreHallViewSet, PlayViewSet, PerformanceViewSet
 
 app_name = "theatre"
 
@@ -9,6 +9,8 @@ router = DefaultRouter()
 router.register("genres", GenreViewSet)
 router.register("actors", ActorViewSet)
 router.register("theatre_halls", TheatreHallViewSet)
+router.register("plays", PlayViewSet)
+router.register("performances", PerformanceViewSet)
 
 urlpatterns = [
     path("", include(router.urls))
