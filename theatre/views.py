34b9f42a3ca9e438c6bfd9b330e7e623864ac481
mkuchuman <mkuchuman1@gmail.com>
2024-05-10 18:36:45 +0200

    added new view

--- theatre/views.py
+++ theatre/views.py
@@ -1,8 +1,11 @@
+from django.db.models import F, Count
 from django.shortcuts import render
 from rest_framework import viewsets, permissions
 
-from theatre.models import Genre, Actor, TheatreHall
-from theatre.serializers import GenreSerializer, ActorSerializer, TheatreHallSerializer
+from theatre.models import Genre, Actor, TheatreHall, Play, Performance
+from theatre.serializers import GenreSerializer, ActorSerializer, TheatreHallSerializer, PlaySerializer, \
+    PlayDetailSerializer, PlayListSerializer, PerformanceListSerializer, PerformanceSerializer, \
+    PerformanceDetailSerializer
 
 
 class GenreViewSet(viewsets.ModelViewSet):
@@ -21,3 +24,38 @@ class TheatreHallViewSet(viewsets.ModelViewSet):
     queryset = TheatreHall.objects.all()
     serializer_class = TheatreHallSerializer
     # permission_classes = ()
+
+
+class PlayViewSet(viewsets.ModelViewSet):
+    queryset = Play.objects.prefetch_related("genres", "actors")
+    serializer_class = PlaySerializer
+    # permission_classes = ()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PlayListSerializer
+        if self.action == "retrieve":
+            return PlayDetailSerializer
+        return PlaySerializer
+
+
+class PerformanceViewSet(viewsets.ModelViewSet):
+    queryset = (
+        Performance.objects.all()
+        .select_related("play", "theatre_hall")
+        .annotate(
+            tickets_available=(
+                F("theatre_hall__rows") * F("theatre_hall__seats_in_row")
+                - Count("tickets")
+            )
+        )
+    )
+    serializer_class = Performance
+    # permission_classes = ()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PerformanceListSerializer
+        if self.action == "retrieve":
+            return PerformanceDetailSerializer
+        return PerformanceSerializer
