7ae65a87ca637fb9162b71512cccec31efb27c63
mkuchuman <mkuchuman1@gmail.com>
2024-05-12 17:32:45 +0200

    include user urls to main urls

--- theatre_service/urls.py
+++ theatre_service/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 urlpatterns = [
     path("__debug__/", include("debug_toolbar.urls")),
     path("admin/", admin.site.urls),
-    path("api/theatre/", include("theatre.urls", namespace="theatre"))
+    path("api/theatre/", include("theatre.urls", namespace="theatre")),
+    path("api/user/", include("user.urls", namespace="user")),
 ]
 
