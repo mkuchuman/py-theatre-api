c06a756ad21bd0c4188001b46bb2d40a23429369
mkuchuman <mkuchuman1@gmail.com>
2024-05-16 14:46:40 +0200

    secret_key now taken from .env

--- theatre_service/settings.py
+++ theatre_service/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-a(y%d@ck%l6$bg-tfa4&5bru&g^^sasze#2syfzk9rt@++4t8_"
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
